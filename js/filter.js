86a62901543c2ed83f0c4218bce6d3b436193f66
ayoubmehd <ayoubelmahdaoui97@gmail.com>
2021-03-03 20:57:56 +0100

    filter(js): fix broken filtering with muliple fileds

--- js/filter.js
+++ js/filter.js
@@ -7,6 +7,7 @@
     event.preventDefault();
 
     Array.from(books.children).forEach((element) => {
+      element.classList.remove("hide");
       authorFilter(element);
       priceFilter(element);
     });
@@ -16,44 +17,45 @@
     const { authorId } = element.querySelector(".author").dataset;
     const select = form.author;
 
-    if (select.value != authorId) {
-      element.style.display = "none";
-    } else {
-      element.style.display = "block";
+    if (select.value != authorId && !element.classList.contains("hide")) {
+      element.classList.add("hide");
     }
-
-    console.log(authorId);
   }
 
   function priceFilter(element) {
+    if (element.classList.contains("hide")) return;
+
     const price = parseFloat(
       element.querySelector(".prix").textContent.replace("dh", "")
     );
     const { min_prix, max_prix } = form;
 
-    if (max_prix.value) {
-      if (price > parseFloat(max_prix.value)) {
-        element.style.display = "none";
-      } else {
-        element.style.display = "flex";
-      }
-    }
-    console.log(
-      `is ${price} less than ${max_prix.value}`,
-      price < parseFloat(max_prix.value)
-    );
+    maxFilter(max_prix, price, element);
+    minFilter(min_prix, price, element);
+  }
 
+  function minFilter(min_prix, price, element) {
     if (min_prix.value) {
       if (price < parseFloat(min_prix.value)) {
         element.style.display = "none";
-      } else {
-        element.style.display = "flex";
       }
     }
 
     console.log(
       `is ${price} bigger than ${min_prix.value}`,
       price > parseFloat(min_prix.value)
+    );
+  }
+
+  function maxFilter(max_prix, price, element) {
+    if (max_prix.value) {
+      if (price > parseFloat(max_prix.value)) {
+        element.style.display = "none";
+      }
+    }
+    console.log(
+      `is ${price} less than ${max_prix.value}`,
+      price < parseFloat(max_prix.value)
     );
   }
 })();
