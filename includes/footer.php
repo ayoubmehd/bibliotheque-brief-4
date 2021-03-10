953afce3c00526675517129174b5f0c4834bb0fc
ayoubmehd <safi-_-gio-raja@live.fr>
2021-03-10 14:21:11 +0100

    footer: add footer html code

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,29 @@
+
+<!--    footer -->
+<div class="footer">
+<div class="footer-con">
+    <div class="icon-sm">
+    <div class="sm">
+        <h3>Restons connectés </h3>
+    </div>
+    <ul class="icon">
+        <li> <a href="#"><img src="/icon/facebook.png"></a></li>
+        <li> <a href="#"><img src="/icon/instagram.png"></a></li>
+        <li> <a href="#"><img src="/icon/linkedin.png"></a></li>
+        <li> <a href="#"><img src="/icon/Twitter.png"></a></li>
+    </ul>
+    </div>
+    <div class="espace-mail">
+    <div class="titre">
+        <h3>Souscrire à notre Newsletter</h3>
+    </div>
+    <div class="New">
+    <input class="Newsletter" placeholder="Saisir votre email" type="Saisir votre email">
+    <button class="Subscribe">Envoyer</button>
+    </div>
+    </div>
+</div>
+</div>
+
 </body>
 </html>
