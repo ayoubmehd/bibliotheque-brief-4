859456e2e27f25dfeb788e7fb82fbfb89399f923
ayoubmehd <safi-_-gio-raja@live.fr>
2021-03-10 14:20:03 +0100

    nav: fix reversing authors and books pages

--- includes/nav.php
+++ includes/nav.php
@@ -8,8 +8,8 @@
     <ul class="nav-menu">
         <li><a href="<?php p_base_url("/index,php") ?>">Home</a></li>
         <li><a href="<?php p_base_url("/gallery.php") ?>">Gallery</a></li>
-        <li><a href="<?php p_base_url("/books.php") ?>">Authors</a></li>
-        <li><a href="<?php p_base_url("/authors.php") ?>">Books</a></li>
+        <li><a href="<?php p_base_url("/authors.php") ?>">Authors</a></li>
+        <li><a href="<?php p_base_url("/books.php") ?>">Books</a></li>
     </ul>
     <div class="cta_wrapper">
         <a href="<?php p_base_url("/contact.php") ?>" class="cta">Contact</a>
