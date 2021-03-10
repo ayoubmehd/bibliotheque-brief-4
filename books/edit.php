e0d8b716ae5a70aa438a40b0af7910237f465920
ayoubmehd <safi-_-gio-raja@live.fr>
2021-03-10 11:30:37 +0100

    books: Create Update , Display

--- books/edit.php
+++ books/edit.php
@@ -1,17 +1,20 @@
 <?php
-include 'connectdb.php';
-$eid= $_GET["eid"];
-$result=$connectdb->prepare("SELECT * FROM livre WHERE id=$eid");
-$result->execute();
+    require_once __DIR__ . "/../includes/header.php";
 
-$row=$result->fetch(PDO::FETCH_ASSOC);
-        extract($row);
-        ?>
-        <form action="editprocess.php" method="post">
-            <input type="hidden" value ="<?php echo $id;?>" name="id">
-            <input type="name" value ="<?php echo $auteur;?>" name="auteur">
-            <input type="name" value ="<?php echo $titre;?>" name="titre">            
-            <input type="file" value="<?php echo "<img src='image/".$cover."'>";?>" name="cover">
-            <input type="text" value ="<?php echo $prix;?>" name="prix">
-            <input type="submit" name="update" value="apdate">
-        </form>
+    require_file("/includes/nav.php");
+    $eid= $_GET["eid"];
+    $result=$connectdb->prepare("SELECT * FROM livre WHERE id=$eid");
+    $result->execute();
+
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    extract($row);
+?>
+    <form action="editprocess.php?redirect=<?php p_base_url("/books.php") ?>" method="post">
+        <input type="hidden" value ="<?php echo $id;?>" name="id">
+        <input type="name" value ="<?php echo $titre;?>" name="titre">            
+        <input type="file" value="<?php echo "<img src='image/".$cover."'>";?>" name="cover">
+        <input type="text" value ="<?php echo $prix;?>" name="prix">
+        <input type="submit" name="update" value="apdate">
+    </form>
+
+<?php require_file("/includes/footer.php") ?>
