afc0de5ab20dbb953628ce2b277db10e0fefe25f
ayoubmehd <safi-_-gio-raja@live.fr>
2021-03-12 10:12:48 +0100

    php helpers: create functions >  to add relation between livre(book) auteur(author) > to remove relation between livre(book) auteur(author)

--- helpers/functions.php
+++ helpers/functions.php
@@ -18,9 +18,13 @@ function dump($value) {
     echo "</pre>";
 }
 
-function require_file($path) {
+function require_file($path, $data = []) {
 
     $path = ROOT . "\\" . $path;
+
+    if(!empty($data)) {
+        extract($data);
+    }
 
     if (file_exists($path)) {
         return require_once($path);
@@ -33,4 +37,36 @@ function redirect($location = '/') {
     $location = isset($_GET["redirect"]) ? $_GET["redirect"] : "/";
 
     header("Location: "  . $location);
+}
+
+// Database
+
+function add_book_author_relation($author_id, $book_id) {
+
+    global $connectdb;
+
+    $result=$connectdb->prepare('INSERT INTO auteur_livre(`id_auteur`, `id_livre`) VALUES(?, ?)');
+    $result->execute([$author_id, $book_id]);
+
+    return $result;
+
+}
+
+function remove_book_author_relaten($author_id, $book_id) {
+
+    global $connectdb;
+
+    $result=$connectdb->prepare('DELETE FROM auteur_livre WHERE id_auteur = ? AND id_livre = ?');
+    $result->execute([$author_id, $book_id]);
+
+    return $result;
+
+}
+
+// Upload
+
+function upload() {
+
+
+    
 }
