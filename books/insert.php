a53375f02d1eddd51efe44e5d63e031cebb652e9
ayoubmehd <safi-_-gio-raja@live.fr>
2021-03-15 16:37:29 +0100

    books: insert auteurs of an inserted book

--- books/insert.php
+++ books/insert.php
@@ -3,13 +3,20 @@
 
     if(isset($_POST['submit']))
     {
-        $auteur=$_POST['auteur'];
         $titre=$_POST['titre'];
         $prix=$_POST['prix'];
 
         $cover = upload("/img-books/gallery/", "cover");
         $result=$connectdb->prepare("INSERT into livre(cover, titre, prix) values(?, ?, ?)");
         $result->execute([$cover, $titre, $prix]);
+
+        $id = $connectdb->lastInsertId();
+
+        // dump($_POST["authors"]);
+        foreach ($_POST["authors"] as $id_auteur) {
+            add_book_author_relation($id_auteur, $id);
+        }
+
 
         redirect();
     }
